6225d1c91f1eee3d4d52ffe4b07115448d311198
Dan Kochetov <danil.kochetov@gmail.com>
2024-04-21 17:40:29 +0300

    Fix build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,76 +1,28 @@
-#!/usr/bin/env -S pnpm tsx
 import 'zx/globals';
-import cpy from 'cpy';
 
-async function updateAndCopyPackageJson() {
-	const pkg = await fs.readJSON('package.json');
+import { build } from 'tsup';
 
-	const entries = await glob('src/**/*.ts');
+fs.removeSync('dist');
 
-	pkg.exports = entries.reduce<
-		Record<
-			string,
-			{
-				import: {
-					types?: string;
-					default: string;
-				};
-				require: {
-					types: string;
-					default: string;
-				};
-				default: string;
-				types: string;
-			}
-		>
-	>((acc, rawEntry) => {
-		const entry = rawEntry.match(/src\/(.*)\.ts/)![1]!;
-		const exportsEntry = entry === 'index' ? '.' : './' + entry.replace(/\/index$/, '');
-		const importEntry = `./${entry}.js`;
-		const requireEntry = `./${entry}.cjs`;
-		acc[exportsEntry] = {
-			import: {
-				types: `./${entry}.d.ts`,
-				default: importEntry,
-			},
-			require: {
-				types: `./${entry}.d.cts`,
-				default: requireEntry,
-			},
-			types: `./${entry}.d.ts`,
-			default: importEntry,
+await build({
+	entry: ['src/index.ts'],
+	splitting: false,
+	sourcemap: true,
+	dts: true,
+	format: ['cjs', 'esm'],
+	outExtension(ctx) {
+		if (ctx.format === 'cjs') {
+			return {
+				dts: '.d.cts',
+				js: '.cjs',
+			};
+		}
+		return {
+			dts: '.d.ts',
+			js: '.js',
 		};
-		return acc;
-	}, {});
+	},
+});
 
-	await fs.writeJSON('dist.new/package.json', pkg, { spaces: 4 });
-}
-
-await fs.remove('dist.new');
-
-await Promise.all([
-	(async () => {
-		await $`tsup`;
-	})(),
-	(async () => {
-		await $`tsc -p tsconfig.dts.json`;
-		await cpy('dist-dts/**/*.d.ts', 'dist.new', {
-			rename: (basename) => basename.replace(/\.d\.ts$/, '.d.cts'),
-		});
-		await cpy('dist-dts/**/*.d.ts', 'dist.new', {
-			rename: (basename) => basename.replace(/\.d\.ts$/, '.d.ts'),
-		});
-	})(),
-]);
-
-await Promise.all([
-	$`tsup src/version.ts --no-config --dts --format esm --outDir dist.new`,
-	$`tsup src/version.ts --no-config --dts --format cjs --outDir dist.new`,
-]);
-
-await $`pnpm tsx scripts/fix-imports.ts`;
-
-await fs.copy('./README.md', 'dist.new/README.md');
-await updateAndCopyPackageJson();
-await fs.remove('dist');
-await fs.rename('dist.new', 'dist');
+fs.copyFileSync('package.json', 'dist/package.json');
+fs.copyFileSync('README.md', 'dist/README.md');
--- src/version.ts
+++
@@ -1,2 +0,0 @@
-// @ts-ignore - imported using Rollup json plugin
-export { version as npmVersion } from '../package.json';
